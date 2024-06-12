7f0608daae1fd2c3e122a2e27f9cf95607c89d2d
Pedro P Camellon <pedropcamellon@gmail.com>
2024-06-12 18:58:15 -0400

    Fix task creation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,21 @@
 class TasksController < ApplicationController
-  before_action :set_project, only: %i[ create, destroy ]
+  before_action :set_project, only: [ :create, :destroy ]
   before_action :set_task, only: %i[ destroy ]
 
   # POST /tasks or /tasks.json
   def create
-    @task = @project.tasks.create(task_params)
-    redirect_to @project
+    @task = @project.tasks.create(task_params.merge(user_id: current_user.id))
+
+    respond_to do |format|
+      if @task.save
+        format.html { redirect_to project_url(@project), notice: "Task was successfully created." }
+      else
+        # Log error
+        puts @task.errors.full_messages.join(', ')
+
+        format.html { redirect_to project_url(@project), notice: "There was an error creating the task." }
+      end
+    end
   end
 
   # DELETE /tasks/1 or /tasks/1.json
